65b329edceaf4cb65057023c8d8896fe84f7d866
Mawfi Islam <113528222+M4WF1@users.noreply.github.com>
2023-09-10 01:06:37 +0600

    Create ictf.py

---
+++ ictf.py
@@ -0,0 +1,3 @@
+print("""Welcome to ICTF 2023.
+Invisible CTF hosts by Invisible Hunter every year in 18 September. You are very lucky because you found it. But this is not the actual file which you wanted.
+Stay with Invisible Hunter""")
